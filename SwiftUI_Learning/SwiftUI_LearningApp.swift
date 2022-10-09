59251c7df286af07c98f4fc3f4022e90a0f22c4d
Oksana Polaikova <it.oksana.poliakova@gmail.com>
2022-10-09 12:03:39 +0300

    Initial Commit

---
+++ SwiftUI_Learning/SwiftUI_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LearningApp.swift
+//  SwiftUI_Learning
+//
+//  Created by Oksana on 09.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
